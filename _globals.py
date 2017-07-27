705fe91c0dbdab7b643fb19ea153736edd2b2e10
Paul Cooper <lucidmotifs@gmail.com>
2017-07-27 14:51:54 +0200

    adding unmerged files from restart feature branch

---
+++ _globals.py
@@ -0,0 +1,79 @@
+import pyautogui
+import keyboard
+import psutil
+
+from timeit import default_timer as timer
+from pywinauto import application
+
+
+KEYBINDS = {
+    "LL" : "q",
+    "UL" : "1",
+    "MID": "2",
+    "UR" : "3",
+    "LR" : "e",
+}
+# shorthand
+K = KEYBINDS
+
+
+def register_keybinds(rotation):
+    for bind,key in KEYBINDS.items():
+        keyboard.add_hotkey(key, rotation.log_keypress, \
+            args=["{} attack key was pressed".format(bind), key])
+
+
+def deregister_keybinds():
+    for bind,key in KEYBINDS.items():
+        keyboard.remove_hotkey(key)
+
+# Process name to switch to
+PROCNAME = "AgeOfConanDX10.exe"
+
+# Attack intervals for various weapons
+attack_int_override = None # If this is not none, will override all others.
+attack_int_1he = .75 # One Handed Edge (not dual-wield)
+
+# Time to wait after hotkey for first step
+opener_wait = .3
+
+# Debugging = True will print thigs to the console
+DEBUG = False
+
+def _set_focus():
+    for proc in psutil.process_iter():
+        if proc.name() == PROCNAME:
+            PID = proc.pid
+
+    app = application.Application()
+    app = app.connect(process=PID)
+    window = app.top_window_()
+    window.Minimize()
+    window.Restore()
+    #window.SetFocus()
+
+
+def _lose_focus():
+    for proc in psutil.process_iter():
+        if proc.name() == PROCNAME:
+            PID = proc.pid
+    app = application.Application()
+    app = app.connect(process=PID)
+    window = app.top_window_()
+    window.Minimize()
+
+
+LAST_KEY_EVENT = 0.0
+def dump_key_event(event):
+    global LAST_KEY_EVENT
+    #print(event.event_type)
+    #print(event.name)
+    print(event.scan_code)
+    if event.event_type is 'down':
+        print(round(event.time - LAST_KEY_EVENT, 2))
+        LAST_KEY_EVENT = event.timeq
+
+
+def dump_keys():
+    LAST_KEY_EVENT = 0.0
+    hk2 = keyboard.hook(dump_key_event)
---
+++ testing/test_basic_combo.py
@@ -0,0 +1,102 @@
+import sys, os
+import unittest
+#print(os.path.abspath('..'))
+#sys.path.insert(0, os.path.abspath('..'))
+
+
+# general utltily
+import logging
+import random
+import time
+import unittest
+import threading
+import queue
+import test.support
+
+# keyboard hooks
+import keyboard
+import pyautogui
+import pywinauto
+
+# timing module
+from timeit import default_timer as timer
+
+# from this application
+#import ipdb; ipdb.set_trace()
+sys.path.append(u"C:/Users/paulcooper/Documents/GitHub/auto-aoc")
+import _globals
+from rotation import Rotation
+from ability import Ability
+from ability import COOLDOWN_ACTIONS
+from combo import Combo
+
+logging.basicConfig(
+    format='(%(threadName)-10s) %(asctime)s.%(msecs)03d %(message)s',
+    datefmt = '%M:%S',
+    filename='testing.log',
+    filemode='w',
+    level=logging.DEBUG)
+
+
+class ActivateChat(Ability):
+
+    def __init__(self):
+        super().__init__("Enter Chat", 0.0)
+        self.hotkey = 'enter'
+
+_enter = ActivateChat()
+
+class ComboTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self._rotation = Rotation()
+        _globals.register_keybinds(self._rotation)
+
+
+    def tearDown(self):
+        self._rotation.end_destructive()
+        Rotation.ability_q = queue.Queue(5)
+        Rotation.combo_q = queue.Queue(1)
+
+        keyboard.unhook_all()
+
+
+    def test_combo_word(self):
+        # BloodyHack6
+        from conqueror.combos import BloodyHack
+        combo = BloodyHack(6)
+
+        self._rotation.use( combo ).at()
+        self._rotation.start_workers()
+        _globals.attack_int_override = 0.2
+
+        rotation_thread = threading.Thread( \
+                                          target=self._rotation.do_start)
+
+        rotation_thread.start()
+        rotation_thread.join()
+
+        self.assertEqual(''.join(self._rotation._keys_pressed),
+                         ''.join(combo.word))
+
+
+    def test_combo_ouput(self):
+        from conqueror.combos import Whirlwind
+        combo = Whirlwind()
+
+        self._rotation.use( combo ).at()
+        self._rotation.start_workers()
+        _globals.attack_int_override = 0.2
+
+        rotation_thread = threading.Thread( \
+                                          target=self._rotation.do_start)
+
+        rotation_thread.start()
+        rotation_thread.join()
+
+        # expected output
+        expected = combo.hotkey
+        for s in combo.steps:
+            expected += s
+
+        self.assertEqual(''.join(self._rotation._keys_pressed), expected)
